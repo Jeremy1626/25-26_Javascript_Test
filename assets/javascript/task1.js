57d78f40bb0ea5c0fa842f37f40999aa2e5b3b8e
13150 <jeremy.garcia@vtsd.com>
2025-12-12 09:24:52 -0500

    task 1

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,16 @@
 /* Put code for the task below */
+
+
+function functionName(){
+    document.getElementById("imageTask1").style.visibility = "visible"
+}
+
+
+function functionName(){
+    document.getElementById("imageTask1").style.visibility = "hidden";
+}
+
+
+
+
+
